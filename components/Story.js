a967fd1cd25bd1639a73db2cd26c16396605bf38
aldana.michelino <aldana.michelino@gmail.com>
2020-09-29 10:05:43 -0300

    cuarto commit

--- components/Story.js
+++ components/Story.js
@@ -2,7 +2,7 @@ export default function Story(story) {
     console.log(story);
     return `<div class="story">
         <div>
-            <span class="gray">${story.index}</span>
+            <span class="gray">${story.index || ""}</span>
             <span class="upvote">▲</span>
             <a href="${story.url}">${story.title}</a>
             <span>(${story.domain})</span>
@@ -11,7 +11,7 @@ export default function Story(story) {
             <div class="gray">
                 ${story.points} points by ${story.user} ${story.time_ago}
                 |
-                <a href="·/item?id=${story.id}">
+                <a href="#/item?id=${story.id}">
                     ${story.comments_count} comments
                 </a>
                 |
